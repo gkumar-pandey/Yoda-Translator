8f5ed52778652bf5e22107153e4db76da658b70b
gkumar-pandey <gautamshekhar078@gmail.com>
2022-07-31 23:21:07 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const inputText = document.querySelector("#text-area");
+const btn = document.querySelector(".btn");
+const outputText = document.querySelector(".output");
+
+const yodaUrl = "https://api.funtranslations.com/translate/yoda.json";
+
+const translateText = async (text) => {
+  try {
+    const url = `${yodaUrl}?text=${text}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    const translatedText = data.contents.translated;
+    outputText.innerHTML = `<p>${translatedText}</p>`;
+  } catch (error) {
+    return null;
+  }
+};
+
+btn.addEventListener("click", () => {
+  if (inputText.value == "") {
+    alert("Please Enter text ");
+  } else {
+    translateText(inputText.value);
+  }
+});
